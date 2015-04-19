05d06abd8881e6d1f2ae6d41e3ff2b4deda244a2
unknown <martinbrambati@gmail.com>
2015-04-19 20:20:08 -0300

    Buy limit

--- new-test.py
+++ new-test.py
@@ -13,6 +13,10 @@ bit = Bittrex(api_key='748fdc31633644849231b13a9f2cafdd', api_secret='25e3129bc6
 # Ticker list example
 
 #firs_market = market_list['result'][0]['MarketName']
-ticker_for_the_first_market = bit.get_ticker('BTC-CRAVE')
+#ticker_for_the_first_market = bit.get_ticker('BTC-CRAVE')
 
-print (ticker_for_the_first_market)
+#print (ticker_for_the_first_market)
+
+# Buy limit example
+limit = bit.buy_limit('BTC-CRAVE', float(0.1) , float(2.1))
+print(limit)
