2c1406a0e86d3ce249561b56fe8d90a4dcf75f6a
unknown <martinbrambati@gmail.com>
2015-04-19 19:54:54 -0300

    Fixed marketname

--- new-test.py
+++ new-test.py
@@ -5,12 +5,14 @@ bit = Bittrex(api_key='748fdc31633644849231b13a9f2cafdd', api_secret='25e3129bc6
 # Only one instance is ok
 
 # Function get_markets
-market_list = bit.get_markets()
+
+#market_list = bit.get_markets()
 # Uncomment next line to print
 # print(market_list)
 
 # Ticker list example
-firs_market = market_list['result'][0]['MarketName']
-ticker_for_the_first_market = bit.get_ticker(firs_market)
+
+#firs_market = market_list['result'][0]['MarketName']
+ticker_for_the_first_market = bit.get_ticker('BTC-CRAVE')
 
 print (ticker_for_the_first_market)
